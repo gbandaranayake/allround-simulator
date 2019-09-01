670b62e352cab94f65ef96638547e2ebe0b73b5d
infamouspotato <ibgayan90@gmail.com>
2019-09-01 21:09:46 +0530

    added table rendering with action buttons

--- simulator-ui/src/collection/CollectionTable.js
+++ simulator-ui/src/collection/CollectionTable.js
@@ -1,17 +1,54 @@
 import React from "react"
 import Table from "react-bootstrap/Table";
-import ButtonToolbar from "react-bootstrap/ButtonToolbar";
-import Button from "react-bootstrap/Button";
-import {FaTrashAlt} from "react-icons/fa";
 import {FaBoxOpen} from "react-icons/fa";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Fade from "react-bootstrap/Fade";
+import Button from "react-bootstrap/Button";
+import {FaTrashAlt} from "react-icons/fa";
+import Tooltip from "react-bootstrap/Tooltip";
 
 class CollectionTable extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {rows: props.rows};
-
+        this.state = {rows: []};
+        this.fetchCollections = this.fetchCollections.bind(this);
         this.deleteRow = this.deleteRow.bind(this);
+    }
+
+    fetchCollections = () => {
+        try {
+            console.log('loading');
+            let url = 'http://127.0.0.1:8080/collections/all';
+            fetch(url).then(response => {
+                let collections = response.json();
+                this.setState({
+                    rows: collections
+                });
+            }, error => {
+                console.log('failed to fetch the collections list ' + this);
+                this.setState({
+                    rows: [{
+                        id: 0,
+                        name: 'This is the collection name',
+                        description: 'This is the collection description'
+                    }]
+                });
+            });
+        } catch (e) {
+            console.log('failed to fetch the collections list');
+        }
+    };
+
+    componentDidMount() {
+        this.fetchCollections();
+        this.timerId = setInterval(
+            () => this.fetchCollections(),
+            60000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
     }
 
     deleteRow(rowId) {
@@ -19,63 +56,57 @@ class CollectionTable extends React.Component {
     }
 
     render() {
+        if (this.state.rows.length === 0) {
+            return null;
+        }
         const tableRowElements = this.state.rows.map((collection) =>
-            <tr>
-                <td>collection.id</td>
-                <td>collection.name</td>
-                <td>collection.description</td>
-                <td>
-                    <ButtonToolbar>
-                        <OverlayTrigger
-                            placement="right-start"
-                            delay={{show: 250, hide: 400}}
-                            overlay={props => <div
-                                {...props}
-                                style={{
-                                    backgroundColor: 'rgba(0, 0, 0, 0.85)',
-                                    padding: '2px 10px',
-                                    color: 'white',
-                                    borderRadius: 3,
-                                    ...props.style,
-                                }}
-                            >
-                                Open
-                            </div>
-                            }
-                        >
-                            <Button variant="success"><FaBoxOpen/></Button>
-                        </OverlayTrigger>
-                        <OverlayTrigger
-                            placement="right-start"
-                            delay={{show: 250, hide: 400}}
-                            overlay={props => <div
-                                {...props}
-                                style={{
-                                    backgroundColor: 'rgba(0, 0, 0, 0.85)',
-                                    padding: '2px 10px',
-                                    color: 'white',
-                                    borderRadius: 3,
-                                    ...props.style,
-                                }}
-                            >
-                                Delete
-                            </div>
-                            }
-                        >
-                            <Button variant="danger" onClick={this.delete(collection.id)}><FaTrashAlt/></Button>
-                        </OverlayTrigger>
-                    </ButtonToolbar>
+            <tr key={collection.id}>
+                <Fade in={true} appear={true} timeout={5000} duration={5000}>
+                    <td>{collection.id}</td>
+                </Fade>
+                <Fade in={true} appear={true} timeout={5000} duration={5000}>
+                    <td>{collection.name}</td>
+                </Fade>
+                <Fade in={true} appear={true} timeout={5000} duration={5000}>
+                    <td>{collection.description}</td>
+                </Fade>
+                <td style={{textAlign: "center"}}>
+                    <OverlayTrigger
+                        key="open-collection"
+                        placement="bottom"
+                        overlay={
+                            <Tooltip id={`tooltip-open-collection`}>
+                                Open collection
+                            </Tooltip>
+                        }
+                    >
+                        <Button className="btn"
+                                style={{'backgroundColor': 'transparent', 'border': 'none'}}><FaBoxOpen/></Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        key="delete-collection"
+                        placement="bottom"
+                        overlay={
+                            <Tooltip id={`tooltip-delete-collection`}>
+                                Delete collection
+                            </Tooltip>
+                        }
+                    >
+                        <Button className="btn" style={{'backgroundColor': 'transparent', 'border': 'none'}}
+                                onClick={() => this.deleteRow(collection.id)} ><FaTrashAlt/></Button>
+                    </OverlayTrigger>
                 </td>
-            </tr>);
+            </tr>
+        );
 
         return (
-            <Table striped bordered hover variant="dark">
+            <Table bordered hover variant="dark" size="sm">
                 <thead>
                 <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Actions</th>
+                    <th className="font-weight-normal">ID</th>
+                    <th className="font-weight-normal">Name</th>
+                    <th className="font-weight-normal">Description</th>
+                    <th className="font-weight-normal text-center">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
--- simulator-ui/src/collection/Collections.js
+++ simulator-ui/src/collection/Collections.js
@@ -7,30 +7,7 @@ import CollectionInfoInput from "./CollectionInfoInput";
 class Collections extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {collections : []};
 
-        this.fetchCollections = this.fetchCollections.bind(this);
-    }
-
-    fetchCollections = () =>{
-        let url = 'http://127.0.0.1:8080/collections/all';
-        fetch(url).
-        then(response => response.json()).then((collections) => {
-            this.setState({
-                collections: collections
-            });
-        });
-    };
-
-    componentDidMount() {
-        this.timerId = setInterval(
-            () => this.fetchCollections(),
-            5000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerId);
     }
 
     render() {
@@ -42,7 +19,7 @@ class Collections extends React.Component{
                         <CollectionInfoInput/>
                     </Jumbotron>
                     <Jumbotron>
-                        <CollectionTable rows={this.state.collections}/>
+                        <CollectionTable/>
                     </Jumbotron>
                 </div>
             </Container>
