614a1da905d7934ce23fb4cf9b2701b9981b3e47
infamouspotato <ibgayan90@gmail.com>
2019-09-08 20:38:02 +0530

    removed outline of toggle button toolset for request method

--- simulator-ui/src/http/client/Requests.js
+++ simulator-ui/src/http/client/Requests.js
@@ -6,33 +6,30 @@ import InputGroup from "react-bootstrap/InputGroup";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
-
-const radioButtonStyles = {
-  outline: 'none',
-};
+import styles from '../../Custom.css';
 
 class Requests extends React.Component {
     render() {
         return (
             <div className="mt-4">
-                <Form>
+                <Form onSubmit={(e) => e.preventDefault()}>
+                    <Form.Group controlId="method">
+                        <InputGroup className="mb-3 col-sm-6">
+                            <ButtonToolbar>
+                                <ToggleButtonGroup type="radio" name="request-method" defaultValue={'GET'}>
+                                    <ToggleButton value={'GET'} variant={'success'}>GET</ToggleButton>
+                                    <ToggleButton value={'POST'} variant={'success'}>POST</ToggleButton>
+                                    <ToggleButton value={'DELETE'} variant={'success'}>DELETE</ToggleButton>
+                                </ToggleButtonGroup>
+                            </ButtonToolbar>
+                        </InputGroup>
+                    </Form.Group>
                     <Form.Group controlId="host">
                         <InputGroup className="mb-3 col-sm-6">
                             <InputGroup.Prepend>
                                 <InputGroup.Text id="uri">URI</InputGroup.Text>
                             </InputGroup.Prepend>
                             <FormControl placeholder="Paste URI here.." aria-label="Host" aria-describedby="uri"/>
-                        </InputGroup>
-                    </Form.Group>
-                    <Form.Group controlId="method">
-                        <InputGroup className="mb-3 col-sm-6">
-                            <ButtonToolbar>
-                                <ToggleButtonGroup type="radio" name="request-method" defaultValue={'GET'}>
-                                    <ToggleButton value={'GET'} variant={'success'} style={radioButtonStyles}>GET</ToggleButton>
-                                    <ToggleButton value={'POST'} variant={'success'}>POST</ToggleButton>
-                                    <ToggleButton value={'DELETE'} variant={'success'}>DELETE</ToggleButton>
-                                </ToggleButtonGroup>
-                            </ButtonToolbar>
                         </InputGroup>
                     </Form.Group>
                     <Button variant="success" type="submit">
